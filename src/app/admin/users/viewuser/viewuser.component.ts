5b7dabfe28b64e727c8abed608e2cbb4f2cfe84e
dam-brz <dam-brz@wp.pl>
2021-08-31 22:40:19 +0200

    Define the deleteUser function to delete selected user

--- src/app/admin/users/viewuser/viewuser.component.ts
+++ src/app/admin/users/viewuser/viewuser.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/model/user/user.model';
+import { HttpClientService } from 'src/app/service/http-client.service';
 
 @Component({
   selector: 'app-viewuser',
@@ -11,9 +13,20 @@ export class ViewuserComponent implements OnInit {
   @Input()
   user!: User
 
-  constructor() { }
+  constructor(
+    private httpClientService: HttpClientService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  deleteUser() {
+    this.httpClientService.deleteUser(this.user.id).subscribe(
+      (user) => {
+        this.router.navigate(['admin', 'users']);
+      }
+    );
   }
 
 }
