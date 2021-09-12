18e5ddae4c7a02a61de8b02b92fa9b2bf18b6ebe
dam-brz <dam-brz@wp.pl>
2021-09-12 10:57:26 +0200

    Create path for the shop component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BooksComponent } from './admin/books/books.component';
 import { UsersComponent } from './admin/users/users.component';
+import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   { path: 'admin/users', component: UsersComponent },
-  { path: 'admin/books', component: BooksComponent }
+  { path: 'admin/books', component: BooksComponent },
+  { path: 'shop', component: ShopComponent }
 ];
 
 @NgModule({
